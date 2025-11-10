c248868fad769ba50dff2335452d1c22c08b243d
emberow <chin45630@gmail.com>
2025-11-10 11:05:55 +0800

    fix: cors issue

--- algo-crack-backend/src/main.ts
+++ algo-crack-backend/src/main.ts
@@ -7,6 +7,13 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: ['http://localhost:8000'], // 前端的網址
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   const config = new DocumentBuilder()
     .setTitle('My API')
     .setDescription('The API description')
